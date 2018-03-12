ad003e84c26332a0ae49bb920b3560a96a6d9653
tianyuningmou <tianyuningmou2009@126.com>
2018-03-12 11:57:30 +0800

    dynamic programme

---
+++ dynamic_progamme/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: __init__.py.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/3/12 上午11:54
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/3/12 上午11:54
+"""
+
+# 动态规划的问题
