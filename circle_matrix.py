29811cdfad6deed2952d05395431f5649ef52198
tianyuningmou <tianyuningmou2009@126.com>
2018-03-21 15:44:42 +0800

    circle_matrix

--- circle_matrix.py
+++ circle_matrix.py
@@ -20,27 +20,27 @@ MODIFIED: : @Time : 2018/3/21 上午9:51
 
 # 回旋矩阵的生成
 def circle_matrix(n):
-    a = [[0 for col in range(n)] for row in range(n)]
+    circle_matrix = [[0 for col in range(n)] for row in range(n)]
     base = 0
     for k in range(0, n//2):
         for i in range(k, n-k):
             base += 1
-            a[k][i] = base
+            circle_matrix[k][i] = base
         for j in range(k+1, n-1-k):
             base += 1
-            a[j][n-1-k] = base
+            circle_matrix[j][n-1-k] = base
         for i in range(n-1-k, k, -1):
             base += 1
-            a[n-1-k][i] = base
+            circle_matrix[n-1-k][i] = base
         for j in range(n-1-k, k, -1):
             base += 1
-            a[j][k] = base
+            circle_matrix[j][k] = base
     if(n%2 == 1):
         base += 1
-        a[(n-1)//2][(n-1)//2] = base
+        circle_matrix[(n-1)//2][(n-1)//2] = base
     for i in range(0, n):
         for j in range(0, n):
-            print('{}'.format(a[i][j]), end='   ')
+            print('{}'.format(circle_matrix[i][j]), end='   ')
         print('\n')
 
 
