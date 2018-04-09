e0032249a495bc711d5a09edc6c4cb2d3487b2a0
tianyuningmou <tianyuningmou2009@126.com>
2018-04-09 14:16:29 +0800

    __init__

---
+++ string/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: __init__.py.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/4/9 下午2:16
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/4/9 下午2:16
+"""
+
+
