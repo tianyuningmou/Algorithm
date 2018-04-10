4a25ba24677cc07017c6341464f6103c7f41d8fc
tianyuningmou <tianyuningmou2009@126.com>
2018-04-10 17:17:06 +0800

    bubble_sort

---
+++ sorted_algorithm/bubble_sort.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: bubble_sort.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/4/10 下午2:54
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/4/10 下午2:54
+"""
+
+"""
+冒泡排序（从小到大）：
+    ①比较相邻的元素。如果第一个比第二个大，就交换他们
+    ②对每一对相邻元素作同样的工作，除了最后一个
+    ③持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较
+"""
+
+def bubble_sort(sort_list):
+    length = len(sort_list)
+    for index in range(length):
+        for i in range(1, length-index):
+            if sort_list[i - 1] > sort_list[i]:
+                sort_list[i -1], sort_list[i] = sort_list[i], sort_list[i - 1]
+    return sort_list
+
+
+if __name__ == '__main__':
+    list = [2, 5, 6, 3]
+    print(bubble_sort(list))
