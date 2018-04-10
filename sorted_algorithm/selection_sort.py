8b8986a9e4b2579ca8530f4ff2813e80bb919e58
tianyuningmou <tianyuningmou2009@126.com>
2018-04-10 17:23:43 +0800

    selection_sort

---
+++ sorted_algorithm/selection_sort.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: selection_sort.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/4/10 下午5:18
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/4/10 下午5:18
+"""
+
+"""
+选择排序：
+    从待排序的数据元素中选出最小(最大)的元素，放在序列的起始位置，直到全部待排序的数据元素排完
+"""
+
+def selection_sort(sort_list):
+    length = len(sort_list)
+    for index in range(length):
+        for i in range(index, length):
+            if sort_list[index] > sort_list[i]:
+                sort_list[index], sort_list[i] = sort_list[i], sort_list[index]
+    return sort_list
+
+if __name__ == '__main__':
+    list = [3, 6, 4, 1]
+    print(selection_sort(list))
