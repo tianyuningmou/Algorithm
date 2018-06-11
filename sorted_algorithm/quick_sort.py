d0ca68fe2326a83f7287445739af04bc5a46de41
tianyuningmou <tianyuningmou2009@126.com>
2018-06-11 15:50:10 +0800

    quick_sort

--- sorted_algorithm/quick_sort.py
+++ sorted_algorithm/quick_sort.py
@@ -46,6 +46,10 @@ def quick_sort(sort_list, i, j):
     return i
 
 
+quick_sort_brief = lambda array: array if len(array) <= 1 else \
+    quick_sort_brief([item for item in array[1:] if item < array[0]]) + [array[0]] + \
+    quick_sort_brief([item for item in array[1:] if item >= array[0]])
+
 if __name__ == '__main__':
     list = [4, 6, 8, 1, 3]
     quick_sort_all(list, 0, len(list) - 1)
