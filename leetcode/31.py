84f474e921f21a16edc9c3260e665f3cd9a2409e
tianyuningmou <tianyuningmou2016@126.com>
2021-03-29 17:36:18 +0800

    3

--- leetcode/31.py
+++ leetcode/31.py
@@ -37,7 +37,7 @@ class Solution(object):
             i -= 1
         if i >= 0:
             j = len(nums) - 1
-            while j>= 0 and nums[i] >= nums[j]:
+            while j >= 0 and nums[i] >= nums[j]:
                 j -= 1
             nums[i], nums[j] = nums[j], nums[i]
         left, right = i + 1, len(nums) - 1
