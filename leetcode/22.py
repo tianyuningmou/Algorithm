c9e5282533bc81b6e886dab40237a9b0bc2756ae
tianyuningmou <tianyuningmou2016@126.com>
2021-03-29 17:36:04 +0800

    3

---
+++ leetcode/22.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2020
+
+All rights reserved
+
+FILE: 22.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2021/3/29 5:12 下午
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2021/3/29 5:12 下午
+"""
+
+"""
+22. 括号生成
+数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合
+"""
+
+
+class Solution(object):
+
+    @classmethod
+    def generateParenthesis(cls, n: int):
+        ans = []
+
+        def backtrace(S, left, right):
+            if len(S) == 2 * n:
+                ans.append(''.join(S))
+            if left < n:
+                S.append('(')
+                backtrace(S, left + 1, right)
+                S.pop()
+            if right < left:
+                S.append(')')
+                backtrace(S, left, right + 1)
+                S.pop()
+
+        backtrace([], 0, 0)
+        return ans
+
+
+if __name__ == '__main__':
+    print(Solution.generateParenthesis(4))
---
+++ leetcode/31.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2020
+
+All rights reserved
+
+FILE: 31.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2021/3/29 5:35 下午
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2021/3/29 5:35 下午
+"""
+
+"""
+31. 下一个序列
+实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+必须 原地 修改，只允许使用额外常数空间。
+"""
+
+
+class Solution(object):
+
+    @classmethod
+    def nextPermutation(cls, nums) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i >= 0:
+            j = len(nums) - 1
+            while j>= 0 and nums[i] >= nums[j]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
