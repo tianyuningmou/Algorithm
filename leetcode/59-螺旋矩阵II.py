c9fcc89a216268ef0aceb701ffeb4b335368864c
tianyuningmou <tianyuningmou2016@126.com>
2021-04-28 16:20:31 +0800

    leetcode/

--- leetcode/59-螺旋矩阵II.py
+++ leetcode/59-螺旋矩阵II.py
@@ -16,3 +16,25 @@ CHANGED By: : tianyuningmou
 CHANGE:  : 
 MODIFIED: : @Time : 2021/4/28 4:16 下午
 """
+
+"""
+59. 螺旋矩阵II
+"""
+
+
+class Solution(object):
+    def generateMatrix(self, n: int):
+        row, col = n, n
+        visited = [[False] * row for _ in range(col)]
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        directionIndex, row_i, col_j = 0, 0, 0
+        num = 1
+        for i in range(row * col):
+            visited[row_i][col_j] = num
+            newRow, newCol = row_i + directions[directionIndex][0], col_j + directions[directionIndex][1]
+            if not (0 <= newRow < row and 0 <= newCol < col and not visited[newRow][newCol]):
+                directionIndex = (directionIndex + 1) % 4
+            row_i += directions[directionIndex][0]
+            col_j += directions[directionIndex][1]
+            num += 1
+        return visited
